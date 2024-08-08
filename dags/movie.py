f44745ea2e5c678757bc6600d3e98913942dacc1
hun0219 <gsw98000@gmail.com>
2024-08-08 15:08:24 +0900

    py edit

--- dags/movie.py
+++ dags/movie.py
@@ -20,29 +20,32 @@ from airflow.operators.python import (
 
 
 with DAG(
-'pyspark_movie',
+    'pyspark_movie',
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
-default_args={
-'depends_on_past': False,
-'email_on_failure': False,
-'email_on_retry': False,
-'retries': 1,
-'retry_delay': timedelta(minutes=5)
-},
-description='spark movie DAG',
-schedule_interval=timedelta(days=1),
-start_date=datetime(2020, 1, 1),
-end_date=datetime(2020, 1, 1),
-catchup=True,
-tags=['spark'],
+    default_args={
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='spark movie DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2020, 1, 1),
+    end_date=datetime(2020, 1, 5),
+    catchup=True,
+    tags=['spark'],
 ) as dag:
 
 
 
     def repartition(ds_nodash):
         from pyspark_airflow.repartition import repartition
-        df = repartition(load_dt, from_path=arg1):
+        df_row_cnt, read_path, write_path = repartition(ds_nodash)
+        print(f'df_row_cnt:{df_row_cnt}')
+        print(f'read_path:{read_path}')
+        print(f'write_path:{write_path}')
 
 
 
@@ -54,22 +57,25 @@ tags=['spark'],
 
     re_partition = PythonVirtualenvOperator(
             task_id='re.partition',
-            python_callable=fun_ext,
-            requirements=["git+https://github.com/play-gogo/Extract.git@d2/0.1.0"],
+            python_callable=repartition,
+            requirements=["git+https://github.com/hun0219/spark_airflow.git@v0.1.0"],
             system_site_packages=False,
-            op_args=["{{ds_nodash}}"]
     )
 
 
     join_df = BashOperator(
             task_id='join.df',
-            bash_command="echo 'task'"
+            bash_command="""
+            echo "{{ds_nodash}}"
+            """
     )
     
     agg = BashOperator(
             task_id='agg',
-            bash_command="echo 'task'"
-
+            bash_command="""
+            echo "{{ds_nodash}}"
+            """
+    )
 
 
 ################################################3
--- src/pyspark_airflow/repartition.py
+++ src/pyspark_airflow/repartition.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import os
+import shutil
 
-def repartition(load_dt, from_path='/data/movie_data'):
+def repartition(load_dt, from_path='/data/movie_data/extract'):
     home_dir = os.path.expanduser("~")
     read_path = f'{home_dir}/{from_path}/load_dt={load_dt}'
+    write_bash = f'{home_dir}/data/movie_data/repartition'
+    write_path = f'{write_base}/load_dt={load_dt}'
+
     df = pd.read_parquet(read_path)
     df['load_dt'] = load_dt
-    df.to_parquet(write_path, partition_cols=['movieCd','multiMovieYn','repNationCd'])
-    retrun df.size, read_path, f'{write_path}/load_dt={load_dt}'
+    rm_dir(write_path)
+    df.to_parquet(write_path, partition_cols=['load_dt','multiMovieYn','repNationCd'])
+    retrun len(df), read_path, wirte_path
+
+def rm_dir(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
