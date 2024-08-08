7d93028be5c070b9cc2b291820d2d0af033408ae
hun0219 <gsw98000@gmail.com>
2024-08-08 16:05:50 +0900

    edit py

--- src/pyspark_airflow/repartition.py
+++ src/pyspark_airflow/repartition.py
@@ -4,7 +4,7 @@ import shutil
 
 def repartition(load_dt, from_path='/data/movie/data/extract'):
     home_dir = os.path.expanduser("~")
-    read_path = f'{home_dir}/{from_path}/load_dt={load_dt]'
+    read_path = f'{home_dir}/{from_path}/load_dt={load_dt}'
     write_base = f'{home_dir}/data/movie_data/repartition'
     write_path = f'{write_base}/load_dt={load_dt}'
 
